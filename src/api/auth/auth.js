73f08b1bf405f5ea8fa0a79d73b66c726ab2f187
kkkhey <2392376497@qq.com>
2024-09-09 18:56:57 +0800

    Initial commit

---
+++ src/api/auth/auth.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+// 注册
+export function userRegister(userDTO) {
+  return request({
+    url: '/ums/user/register',
+    method: 'post',
+    data: userDTO
+  })
+}
+
+// 前台用户登录
+export function login(data) {
+  return request({
+    url: '/ums/user/login',
+    method: 'post',
+    data
+  })
+}
+// 登录后获取前台用户信息
+export function getUserInfo() {
+  return request({
+    url: '/ums/user/info',
+    method: 'get'
+  })
+}
+// 前台用户注销
+export function logout() {
+  return request({
+    url: '/ums/user/logout'
+  })
+}
+// 管理员登录
+export function adminlogin(data) {
+  return request({
+    url: '/admin/login',
+    method: 'post',
+    data
+  })
+}
